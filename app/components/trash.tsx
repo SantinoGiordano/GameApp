9da96dd56aeec28b2e6dff6a0289b9521efe4f46
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-08 11:40:41 -0400

    creating a trash item for draggable

---
+++ app/components/trash.tsx
@@ -0,0 +1,17 @@
+
+import { DndContext } from "@dnd-kit/core";
+import React from "react";
+
+const Trash = () => {
+  return (
+    <>
+      <DndContext>
+        {/* <DroppableArea id="available" label="Available Items">
+            this
+        </DroppableArea> */}
+      </DndContext>
+    </>
+  );
+};
+
+export default Trash;
