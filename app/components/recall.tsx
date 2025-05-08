00b72233c3fc5339c90382715eec7c5dcc1f6e03
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-08 11:32:02 -0400

    changed button to a red recall

--- app/components/recall.tsx
+++ app/components/recall.tsx
@@ -6,7 +6,7 @@ type RecallProps = {
     return (
       <button
         onClick={onRecall}
-        className="m-4 ml-0 p-5 px-4 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow hover:bg-indigo-700 transition-colors duration-200"
+        className="m-4 ml-0 p-5 px-4 py-2 bg-red-600 text-white font-semibold rounded-lg shadow hover:bg-red-700 transition-colors duration-200"
       >
         Recall
       </button>
