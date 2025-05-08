634d1a0bd5f843ed0b5f93475d65a6673b1bd959
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-08 12:34:49 -0400

    modifyed the trahs comopnent

--- app/components/trash.tsx
+++ app/components/trash.tsx
@@ -1,17 +1,20 @@
-
-import { DndContext } from "@dnd-kit/core";
 import React from "react";
+import { useDroppable } from "@dnd-kit/core";
 
 const Trash = () => {
+  const { setNodeRef, isOver } = useDroppable({ id: "trash" });
+
   return (
-    <>
-      <DndContext>
-        {/* <DroppableArea id="available" label="Available Items">
-            this
-        </DroppableArea> */}
-      </DndContext>
-    </>
+    <div
+      ref={setNodeRef}
+      className={`mt-6 p-6 border-4 border-dashed rounded-lg text-center transition-colors duration-200 ${
+        isOver ? "bg-red-300 border-red-600" : "bg-red-100 border-red-500"
+      }`}
+    >
+      <p className="text-red-700 font-bold text-lg">🗑 Drop Here to Delete</p>
+    </div>
   );
 };
 
 export default Trash;
+
